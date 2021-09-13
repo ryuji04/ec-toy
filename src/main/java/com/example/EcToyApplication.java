319c1352ae45f5115a823881e134d23db18982df
adachiryuji <ryuji.adachi@rakus-partners.co.jp>
2021-09-13 12:35:33 +0900

    first commit

---
+++ src/main/java/com/example/EcToyApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcToyApplication.class, args);
+	}
+
+}
